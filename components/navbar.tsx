9de32912e5181df80b92cf35926393c4787a32bb
Presson-coder <pressonzhou04@gmail.com>
2026-05-11 11:23:41 +0200

    logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
           {/* Logo */}
           <Link href="/">
             <Image 
-              src="/images/dn-logo-2.png" 
+              src="/images/dn-logo-3.png" 
               alt="Dumiso Ngwenyama" 
               width={200} 
               height={50} 
